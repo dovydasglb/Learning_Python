c384a93e39a238a885ef9804060cd19a21791bf7
dovydasglb <dovydasglb@gmail.com>
2022-02-04 14:02:17 +0000

    Created new files

---
+++ collections.py
@@ -0,0 +1,2 @@
+# Collections are 'containers' used to store multiple data entries inside a variable
+# Commonly used collections are: Lists, Tuples, Sets and Dictionaries
---
+++ key_inbuilt_functions.py
@@ -0,0 +1,8 @@
+# Input/output functions allow you to enter and print out data using terminal
+# Input:
+x = input('Please enter a number: ')
+# Output:
+print(x)
+# Note that input function inputs assigns string type by default:
+print(type(x))
+
