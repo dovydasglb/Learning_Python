dfda504e099737ee9c8e914214ec083d8c6faba5
dovydasglb <dovydasglb@gmail.com>
2022-02-04 22:31:25 +0000

    Completed summary of collections

--- collections.py
+++ collections.py
@@ -6,15 +6,27 @@
 my_list = ['a', 3, 'a', True, 2.0]
 print(my_list)
 print(type(my_list))
+# can also be created using:
+my_list = list(('a', 3, 'a', True, 2.0))
+print(my_list)
+print(type(my_list))
 
 # Tuples are created using round brackets (ordered, allow duplicates, unchangeable, allow multiple data types):
 my_tuple = ('a', 'a', 3, True, 2.0)
+print(my_tuple)
+print(type(my_tuple))
+# can also be created using:
+my_tuple = tuple(('a', 'a', 3, True, 2.0))
 print(my_tuple)
 print(type(my_tuple))
 
 # Sets are created using curly brackets (unordered, no duplicates (will only store a single instance of
 # repeated item), unchangeable (except add/delete items) and allow multiple data types:
 my_set = {'a', 'a', 3, True, 2.0}
+print(my_set)
+print(type(my_set))
+# can also be created using:
+my_set = set(('a', 'a', 3, True, 2.0))
 print(my_set)
 print(type(my_set))
 
@@ -28,5 +40,9 @@ my_dictionary = {
     'product': 'iPhone',
     'current model': False
 }
+print(my_dictionary)
+print(type(my_dictionary))
+# can also be created using (note parentheses):
+my_dictionary = dict({1:2020, 2:'September', 'brand':'Apple', 'product':'iPhone', 'current model':False})
 print(my_dictionary)
 print(type(my_dictionary))
