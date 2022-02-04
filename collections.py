7ab271f8bd42ed4df4fd8b73671665e584fe6f8a
dovydasglb <dovydasglb@gmail.com>
2022-02-04 21:50:35 +0000

    Completed summary of built-in collections

--- collections.py
+++ collections.py
@@ -1,2 +1,32 @@
-# Collections are 'containers' used to store multiple data entries inside a variable
-# Commonly used collections are: Lists, Tuples, Sets and Dictionaries
+# Collections (arrays) are 'containers' used to store multiple data entries inside a variable
+# Commonly used Python built-in collections are: Lists, Tuples, Sets and Dictionaries
+
+# Lists are created using square brackets (ordered, allow duplicates, changeable (change/add/remove), allow
+# multiple data types):
+my_list = ['a', 3, 'a', True, 2.0]
+print(my_list)
+print(type(my_list))
+
+# Tuples are created using round brackets (ordered, allow duplicates, unchangeable, allow multiple data types):
+my_tuple = ('a', 'a', 3, True, 2.0)
+print(my_tuple)
+print(type(my_tuple))
+
+# Sets are created using curly brackets (unordered, no duplicates (will only store a single instance of
+# repeated item), unchangeable (except add/delete items) and allow multiple data types:
+my_set = {'a', 'a', 3, True, 2.0}
+print(my_set)
+print(type(my_set))
+
+# Dictionaries are created using curly brackets and contain key:value pairs (ordered, no duplicates (cannot
+# have values with the same key), changeable (change/add/remove), can contain multiple data types (both keys and
+# values)):
+my_dictionary = {
+    1: 2020,
+    2: 'September',
+    'brand': 'Apple',
+    'product': 'iPhone',
+    'current model': False
+}
+print(my_dictionary)
+print(type(my_dictionary))
